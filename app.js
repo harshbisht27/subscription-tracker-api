0523e0711311d69ddedaf6631805e88799ccfa26
harshbisht27 <harshbisht180@gmail.com>
2025-03-13 23:52:35 +0530

    app

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-import express from "express"
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
+});
+
+app.listen(PORT, async () => {
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
+
+});
+
+export default app;
